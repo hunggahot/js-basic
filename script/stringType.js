c1ec2f393ee63afcc94f695c9e5b559799d0271a
hunggahot <hunggahot@gmail.com>
2023-06-22 22:00:48 +0700

    stringType

---
+++ script/stringType.js
@@ -0,0 +1,23 @@
+/*
+Chuỗi trong Javascript
+
+1. Tạo chuỗi
+    - Các cách tạo chuỗi
+    - Nên dùng cách nào? Lý do?
+    - Kiểm tra data type
+
+2. Một số case sử dụng backslash (\)
+
+3. Xem độ dài chuỗi
+
+4. Chú ý độ dài khi viết code
+
+5. Template string ES6
+*/
+
+var userName = 'lamHungw'
+
+var fullName = new String('Lam Quoc Hung')
+
+console.log(userName)
+console.log(`length = ${fullName.length}`)
