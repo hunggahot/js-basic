268a186f3ac36b4c6694e93105f5de5137a5c747
hunggahot <hunggahot@gmail.com>
2023-06-22 21:13:40 +0700

    return

---
+++ script/return.js
@@ -0,0 +1,13 @@
+// Return trong hàm - Javascript cơ bản
+
+// var isConfirm = confirm('Message?')
+
+// console.log(isConfirm)
+
+function sum(a, b) {
+    return a + b
+}
+
+var result = sum(2, 8)
+
+console.log(result)
