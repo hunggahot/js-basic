b0798eaf4f3e3aec2d349b0e2de97aa5028d3202
hunggahot <hunggahot@gmail.com>
2023-06-22 19:49:51 +0700

    function

---
+++ script/main.js
@@ -0,0 +1,57 @@
+// alert('Hi JavaScript basic!');
+
+// var fullName = 'Lam Quoc Hung';
+// var age = 22;
+
+// alert(fullName);
+// alert(age);
+
+// console.log(fullName);
+
+// confirm('Xac nhan ban du tuoi!');
+
+// prompt('Xac nhan ban du tuoi!');
+
+// setTimeout(function() {
+//     alert('Không lặp lại chỉ xảy ra một lần duy nhất');
+// }, 1000);
+
+// setInterval(function() {
+//     // alert('Chạy lặp lại nhiều lần');
+//     console.log('Day la log ' + Math.random());
+// }, 1000);
+
+// var a = 1;
+// var b = 2;
+
+// if(a == b){
+//     alert('Dung');
+// } else{
+//     alert('Sai');
+// }
+
+// var a = 1;
+// var b = 2;
+
+// if(a > 0 && b > 0) {
+//     alert('a & b lon hon 0');
+// }
+
+
+// var a = 8;
+// var b = 3;
+// var c = a % b;
+// console.log(c);
+
+// var a = 5;
+
+// a--;
+// console.log(a);
+// a++;
+// console.log(a);
+
+// var name = 'Hung';
+// name += ' Lam';
+
+// console.log(name);
+
