f924c970f3c3e3b7d7eb7549a027ebe3ae0899ce
hunggahot <hunggahot@gmail.com>
2023-06-24 13:07:29 +0700

    switch

---
+++ script/switch.js
@@ -0,0 +1,25 @@
+// Câu lệnh rẽ nhánh - Switch
+
+var date = 9
+
+switch (date) {
+    case 0:
+    case 1:
+    case 2:
+        console.log('Thu 2, 1, 0')
+        break
+    case 3:
+        console.log('Thu 3')
+        break
+    case 4:
+        console.log('Thu 4')
+        break
+    case 5:
+        console.log('Thu 5')
+        break
+    case 6:
+        console.log('Thu 6')
+        break
+    default:
+        console.log('?')
+}
