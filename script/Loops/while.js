2d7aef3bdb08bfe507b292f766988c496db53e07
hunggahot <hunggahot@gmail.com>
2023-06-24 15:33:12 +0700

    while

---
+++ script/Loops/while.js
@@ -0,0 +1,19 @@
+// While loop
+
+var i = 0
+
+// while (i <= 10) {
+//     console.log(i)
+//     i++
+// }
+
+var myArray = [
+    'Javascript',
+    'PHP',
+    'Java'
+]
+
+while (i < myArray.length) {
+    console.log(myArray[i])
+    i++
+}
