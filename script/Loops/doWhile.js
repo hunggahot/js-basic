08a63118a5fe254a60c04298c09b1b325b5a996c
hunggahot <hunggahot@gmail.com>
2023-06-24 15:43:26 +0700

    doWhile

---
+++ script/Loops/doWhile.js
@@ -0,0 +1,15 @@
+// do/while loop
+
+var i = 0;
+var isSuccess = false
+
+do {
+    // code
+    i++
+    console.log('Nap the lan ' + i)
+
+    // success
+    if (false) {
+        isSuccess = true
+    }
+} while (!isSuccess && i < 3)
