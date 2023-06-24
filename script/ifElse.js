8f3b51842530be912c80c37e9a67b17b17aebde9
hunggahot <hunggahot@gmail.com>
2023-06-24 12:57:47 +0700

    if else

---
+++ script/ifElse.js
@@ -0,0 +1,11 @@
+// Câu lệnh rẽ nhánh - If else
+
+var date = 3;
+
+if (date === 2) {
+    console.log('Thứ 2')
+} else if (date === 3) {
+    console.log('Thứ 3')
+} else {
+    console.log('Thứ 4')
+}
