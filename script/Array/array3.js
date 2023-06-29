c42860ace70eaa8280685a6d5d585b89cb11cbdb
hunggahot <hunggahot@gmail.com>
2023-06-29 17:09:24 +0700

    array3.1

--- script/Array/array3.js
+++ script/Array/array3.js
@@ -192,3 +192,205 @@ function getTotalGold(arr) {
 console.log(getTotalGold(sports)) // Output: 23
 
 
+var totalGold = sports.reduce(function (total, sport) {
+    return total + sport.gold
+}, 0)
+
+console.log(`Tổng số huy chương: ${totalGold}`)
+
+
+var numbers = [200, 300, 0, 400, 500]
+
+var totalCoinNum = numbers.reduce(function (total, number) {
+    return total + number
+})
+
+console.log(totalCoinNum)
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Flat - "Làm phẳng" mảng từ Depth array - "Mảng sâu"
+var depthArray = [1, 2, [3, 4], 5, 6, [7, 8, 9]]
+
+var i = 0
+var flatArray = depthArray.reduce(function (flatOutput, depthItem) {
+    i++
+    console.table({
+        'Lượt chạy ': i,
+        'flatOutput ': flatOutput,
+        'depthItem': depthItem,
+        'result': flatOutput.concat(depthItem)
+    })
+    return flatOutput.concat(depthItem)
+}, [])
+
+console.log(flatArray)
+
+
+// Lấy ra các khóa học đưa vào 1 mảng mới
+var topics1 = [
+    {
+        topic1: "Front-end",
+        courses2: [
+            {
+                id: 1,
+                title: "HTML & CSS"
+            },
+            {
+                id: 2,
+                title: "Javascript"
+            }
+        ]
+    },
+    {
+        topic1: "Back-end",
+        courses2: [
+            {
+                id: 1,
+                title: "PHP"
+            },
+            {
+                id: 2,
+                title: "NodeJS"
+            }
+        ]
+    }
+]
+
+var j = 0
+var newCourses1 = topics1.reduce(function (courses2, topic1) {
+    j++
+    console.table({
+        'Lượt chạy ': j,
+        'course ': courses2,
+        'topic': topic1,
+        'result': courses2.concat(topic1.courses2)
+    })
+    return courses2.concat(topic1.courses2);
+
+}, [])
+
+console.log(newCourses1)
+
+var htmls = newCourses1.map(function (course) {
+    return `
+        <div>
+            <h2>${course.title}<h2>
+            <p>Id: ${course.id}<p>
+        </div>
+    `
+})
+
+console.log(htmls.join(''))
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+// Cho trước danh sách một số bộ phim, hãy viết hàm calculateRating để tính điểm trung bình IMDB của những bộ phim mà Christopher Nolan làm đạo diễn.
+
+
+var watchList = [
+    {
+        "Title": "Inception",
+        "Year": "2010",
+        "Rated": "PG-13",
+        "Released": "16 Jul 2010",
+        "Runtime": "148 min",
+        "Genre": "Action, Adventure, Crime",
+        "Director": "Christopher Nolan",
+        "Writer": "Christopher Nolan",
+        "Actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page, Tom Hardy",
+        "Plot": "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
+        "Language": "English, Japanese, French",
+        "Country": "USA, UK",
+        "imdbRating": "8.8",
+        "imdbVotes": "1,446,708",
+        "imdbID": "tt1375666",
+        "Type": "movie",
+    },
+    {
+        "Title": "Interstellar",
+        "Year": "2014",
+        "Rated": "PG-13",
+        "Released": "07 Nov 2014",
+        "Runtime": "169 min",
+        "Genre": "Adventure, Drama, Sci-Fi",
+        "Director": "Christopher Nolan",
+        "Writer": "Jonathan Nolan, Christopher Nolan",
+        "Actors": "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+        "Plot": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+        "Language": "English",
+        "Country": "USA, UK",
+        "imdbRating": "8.6",
+        "imdbVotes": "910,366",
+        "imdbID": "tt0816692",
+        "Type": "movie",
+    },
+    {
+        "Title": "The Dark Knight",
+        "Year": "2008",
+        "Rated": "PG-13",
+        "Released": "18 Jul 2008",
+        "Runtime": "152 min",
+        "Genre": "Action, Adventure, Crime",
+        "Director": "Christopher Nolan",
+        "Writer": "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
+        "Actors": "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+        "Plot": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+        "Language": "English, Mandarin",
+        "Country": "USA, UK",
+        "imdbRating": "9.0",
+        "imdbVotes": "1,652,832",
+        "imdbID": "tt0468569",
+        "Type": "movie",
+    },
+    {
+        "Title": "Batman Begins",
+        "Year": "2005",
+        "Rated": "PG-13",
+        "Released": "15 Jun 2005",
+        "Runtime": "140 min",
+        "Genre": "Action, Adventure",
+        "Director": "Christopher Nolan",
+        "Writer": "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
+        "Actors": "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
+        "Plot": "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
+        "Language": "English, Urdu, Mandarin",
+        "Country": "USA, UK",
+        "imdbRating": "8.3",
+        "imdbVotes": "972,584",
+        "imdbID": "tt0372784",
+        "Type": "movie",
+    },
+    {
+        "Title": "Avatar",
+        "Year": "2009",
+        "Rated": "PG-13",
+        "Released": "18 Dec 2009",
+        "Runtime": "162 min",
+        "Genre": "Action, Adventure, Fantasy",
+        "Director": "James Cameron",
+        "Writer": "James Cameron",
+        "Actors": "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+        "Plot": "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+        "Language": "English, Spanish",
+        "Country": "USA, UK",
+        "imdbRating": "7.9",
+        "imdbVotes": "876,575",
+        "imdbID": "tt0499549",
+        "Type": "movie",
+    }
+];
+
+function calculateRating(watchList) {
+    var nolanMovies = watchList.filter(function (watchList) {
+        return watchList.Director === 'Christopher Nolan'
+    })
+    var totalIMDB = nolanMovies.reduce(function (a, b) {
+        return (a + Number(b.imdbRating))
+    }, 0)
+    return totalIMDB / nolanMovies.length
+}
+
+// Expected results
+console.log(calculateRating(watchList)); // Output: 8.675
