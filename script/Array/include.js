a95f460f9e547c5fe585044c659f3e0640b32b44
hunggahot <hunggahot@gmail.com>
2023-06-29 17:45:30 +0700

    reduce

---
+++ script/Array/include.js
@@ -0,0 +1,10 @@
+// Includes method: Chỉ được sử dụng cho String và Array
+// Tìm kiếm phần tử trong chuỗi hoặc mảng có tồn tại hay không
+
+var title = 'Responsive web desgin'
+
+console.log(title.includes('Responsive'))
+
+var courses = ['Javascript', 'PHP', 'C#']
+
+console.log(courses.includes('Javascript'))
